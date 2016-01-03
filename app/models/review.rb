699108d26be9384688576505db781ece2f69f2b4
ruiduarte86 <ruibritocamacho@gmail.com>
2016-01-03 23:17:43 +0000

    added average score calculation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,14 @@ class Review < ActiveRecord::Base
     belongs_to :user
     belongs_to :place
 
+    validates_presence_of :score
+    validates_presence_of :content
+    
+    after_save :calculate_average
+
+    
+    def calculate_average
+        a = self.place.average_rating
+        self.place.update_attributes(total_average_rating: a)
+    end
 end
